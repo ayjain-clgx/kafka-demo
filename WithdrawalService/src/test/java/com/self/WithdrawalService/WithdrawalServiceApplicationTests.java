9d7284b89ba5367beb3faacadee2e5607fc01fc4
Ayush Jain <ayjain@cotality.com>
2025-12-04 16:04:16 +0530

    [Kafka Txns] Code for Withdrawl service

---
+++ WithdrawalService/src/test/java/com/self/WithdrawalService/WithdrawalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.WithdrawalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdrawalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
